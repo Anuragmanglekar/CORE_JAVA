3f6aa346675a2577155005cd1a1a1112eaa1117e
Anuragmanglekar <anuragmanglekar@gmail.com>
2026-01-19 20:43:20 +0530

    Day1

---
+++ DAY1/demo1.java
@@ -0,0 +1,9 @@
+package DAY1;
+
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+        System.out.println("This is my first commit");
+    }
+    
+}
