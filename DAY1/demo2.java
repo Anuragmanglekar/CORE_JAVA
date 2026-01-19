b7b18263ee90c9021ef5dde98b651a52b2f27719
Anuragmanglekar <anuragmanglekar@gmail.com>
2026-01-19 21:21:11 +0530

    DAY1

---
+++ DAY1/demo2.java
@@ -0,0 +1,13 @@
+package DAY1;
+
+import java.util.Scanner;
+
+public class demo2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the name: ");
+        String str = sc.next();
+        System.out.print("Entered name: " + str);
+    }
+
+}
